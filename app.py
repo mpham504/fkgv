5370fcb7fc57de4db32655060502cf3f67f899c8
mpham504 <minh.qp@gmail.com>
2025-03-27 18:09:42 -0500

    Fixed payments

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def create_checkout_session():
         
         # Create Stripe Checkout session
         session = stripe.checkout.Session.create(
-            payment_method_types=['card', 'cashapp', 'apple pay', 'google pay'],
+            payment_method_types=['card', 'cashapp'],
             line_items=[
                 {
                     'price_data': {
