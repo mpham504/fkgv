ec6f17546c064e96e8886b8871c2ab3a87f58ff1
mpham504 <minh.qp@gmail.com>
2025-03-27 08:23:38 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -71,3 +71,6 @@ def cancel():
     return "Payment Canceled. Try Again."
 
 # Note: Don't add app.run() here. Gunicorn will handle running the app.
+
+port = os.environ.get('PORT', 5000)  # Default to 5000 for local development
+app.run(debug=False, host='0.0.0.0', port=port)
